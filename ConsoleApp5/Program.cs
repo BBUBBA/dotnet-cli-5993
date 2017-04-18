1d638a41a5c3e4aee922fb9f7ddad59c54c7f842
Roman Guryanov <spotirguryanov@gmail.com>
2017-04-18 21:00:56 +0300

    simple workaround for SINGLE net40 target framework

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+	using System.Threading.Tasks;
+
+	class Program
+	{
+		static void Main() => F().Wait();
+		static async Task F() => await TaskEx.Delay(10000);
+	}
+}
